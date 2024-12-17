d5d033728e0b91eda814aa8415487be0a3e20a4f
Haris Talic <htalic@evoila.com>
2024-12-17 08:34:09 +0100

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    gridDataUrl: 'http://77.78.198.63:252/'
+}
---
+++ src/shared/models/data.ts
@@ -0,0 +1,10 @@
+export interface GridData {
+    id: string;
+    klasifikacija: string;
+    naziv: string;
+    karakteristikaA: string;
+    karakteristikaB: string;
+    karakteristikaC: string;
+    karakteristikaD: string;
+    karakteristikaE: string;
+  }
